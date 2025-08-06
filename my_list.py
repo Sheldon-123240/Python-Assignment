cc5c3d83f0883351f470d6cb55d4556b98019dca
Sheldon-123240 <owinosheldon883@gmail.com>
2025-08-06 15:06:08 +0300

    My second assignment submission

--- my_list.py
+++ my_list.py
@@ -1,1 +1,25 @@
+#My List
+my_list = [ 13, 45 ,64 ,45]
 
+#New List
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print("My_list:", my_list)
+
+my_list.insert(1,15)
+print("My_list:", my_list)
+
+my_list2 = [50, 60, 70]
+
+my_list.extend(my_list2)
+print("My_list:", my_list)
+
+my_list.remove(70)
+print("My_list:", my_list)
+
+my_list.sort() #In ascending order
+print("My_list:", my_list)
+
+print(my_list[4])
